7606c32bb19b7fde42b889fa5d52f28baf92b30d
mvlipka <mvlipka@gmail.com>
2024-05-13 12:09:40 -0500

    added input test

--- pkg/primes/calculators/eratosthenes_test.go
+++ pkg/primes/calculators/eratosthenes_test.go
@@ -14,6 +14,20 @@ func TestEratosthenesCalculator_GeneratePrimesInRange(t *testing.T) {
 	assert.Equal(t, []int64{5, 7, 11, 13}, result)
 }
 
+func TestNewEratosthenesCalculator_GeneratePrimesInRangeInputError(t *testing.T) {
+	calculator := EratosthenesCalculator{}
+
+	result, err := calculator.GeneratePrimesInRange(int64(-1), int64(3))
+	require.Error(t, err)
+	assert.Nil(t, result)
+	assert.Contains(t, err.Error(), "error start of range must be positive")
+
+	result, err = calculator.GeneratePrimesInRange(int64(5), int64(4))
+	require.Error(t, err)
+	assert.Nil(t, result)
+	assert.Contains(t, err.Error(), "error start of range must be less than end of range")
+}
+
 func BenchmarkEratosthenesCalculator_GeneratePrimesInRange(b *testing.B) {
 	calculator := EratosthenesCalculator{}
 
